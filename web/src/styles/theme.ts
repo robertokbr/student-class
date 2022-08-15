e63100359978016fc9eee142fea917dde625ba79
robkbr <roberto.junior@nickelpay.com.br>
2022-08-15 12:32:59 -0300

    chore: update style

--- web/src/styles/theme.ts
+++ web/src/styles/theme.ts
@@ -1,18 +1,20 @@
 import { extendTheme } from '@chakra-ui/react';
 
+const black = "#231F20"
+
 export const dracula = {
   BackgroundPrimary: '#1d1e26',
-  BackgroundSecondary:	'#282a36',	
-  CurrentLine: '#44475a',	
-  Comment:	'#6272a4',	
-  Foreground:	'#f8f8f2',	
-  Cyan:	'#8be9fd',	
-  Green:	'#50fa7b',	
-  Orange:	'#ffb86c',	
-  Pink:	'#ff79c6',	
-  Purple:	'#bd93f9',	
-  Red:	'#ff5555',	
-  Yellow:	'#f1fa8c',	
+  BackgroundSecondary:	'#282a36',
+  CurrentLine: '#44475a',
+  Comment:	'#6272a4',
+  Foreground:	'#f8f8f2',
+  Cyan:	'#8be9fd',
+  Green:	'#50fa7b',
+  Orange:	'#ffb86c',
+  Pink:	'#ff79c6',
+  Purple:	'#bd93f9',
+  Red:	'#ff5555',
+  Yellow:	'#f1fa8c',
 };
 
 export const randomDraculaBackground = () => {
@@ -29,7 +31,7 @@ export const simpleHover = {
 }
 
 export const textGradiant = {
-  bgGradient: "linear(to-b, gray.50, transparent)", 
+  bgGradient: "linear(to-b, gray.50, transparent)",
   bgClip: "text",
 }
 
